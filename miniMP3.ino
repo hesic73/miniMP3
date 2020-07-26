f5d6eb8cb216d58201ed60c895a6c5af30dbc710
hesic73 <hesicheng2001@163.com>
2020-07-26 13:01:36 +0800

    增添了自动播放下一首功能函数autonext

--- miniMP3.ino
+++ miniMP3.ino
@@ -23,16 +23,18 @@ char vol;//音量,用于显示
 unsigned int totalsong;//当前目录总曲目
 unsigned int cur;//当前曲目
 TMRpcm music;
-void isr(){mode=(mode+1)%3;}//简单地切模式
+void isr() {
+  mode = (mode + 1) % 3; //简单地切模式
+}
 void hang();//等待摇杆回到初始状态
 int input();//摇动且回复到初始状态算一次输入
 void autonext();//当一首歌曲播放完毕时，按照模式选择播放下一首歌
 void setup() {
   //串口初始化
-  pinMode(BUTTON,INPUT_PULLUP);
-  pinMode(MODE,INPUT_PULLUP);
-  mode=0;
-  attachInterrupt(1,isr,FALLING);
+  pinMode(BUTTON, INPUT_PULLUP);
+  pinMode(MODE, INPUT_PULLUP);
+  mode = 0;
+  attachInterrupt(1, isr, FALLING);
   //音乐播放初始化
   music.speakerPin = SOUND;
   Serial.begin(9600);
@@ -40,24 +42,24 @@ void setup() {
     Serial.println("SD fail");
     return;
   }
-  vol=5;
-  music.setVolume(vol);   
-  music.quality(1); 
+  vol = 5;
+  music.setVolume(vol);
+  music.quality(1);
   //文件初始化
-  dir=SD.open("/");
-  totalsong=0;
-  while(1){
-    wavfile=dir.openNextFile() ;
-    if(!wavfile) break;
+  dir = SD.open("/");
+  totalsong = 0;
+  while (1) {
+    wavfile = dir.openNextFile() ;
+    if (!wavfile) break;
     totalsong++;
     wavfile.close();
   }
-  wavfile=dir.rewindDirectory();
-  if(totalsong==0){
+  wavfile = dir.rewindDirectory();
+  if (totalsong == 0) {
     Serial.println("Empty directory.");
     return ;
   }
-  cur=1;
+  cur = 1;
   music.play(wavfile.name());//播放第一首曲子
   wavfile.close();
 }
@@ -68,43 +70,43 @@ void loop() {
     case PAUSE: music.pause(); break;
     case PREV: break;
     case NEXT: break;
-    case VOL_UP: vol=MAX(vol+1,7);music.setVolume(vol);break;
-    case VOL_DN: vol=MIN(vol-1,0);music.setVolume(vol);break;
+    case VOL_UP: vol = MAX(vol + 1, 7); music.setVolume(vol); break;
+    case VOL_DN: vol = MIN(vol - 1, 0); music.setVolume(vol); break;
   }
 }
 void hang()
 {
-  int x,y,b;
-  while(1){//也要考虑等的过程中歌曲完毕的情况？
+  int x, y, b;
+  while (1) { //也要考虑等的过程中歌曲完毕的情况？
     b = digitalRead(BUTTON);
     x = analogRead(VX);
     y = analogRead(VY);
-    if(b&&(x>400&&x<600)&&(y>400&&y<600)) break;
+    if (b && (x > 400 && x < 600) && (y > 400 && y < 600)) break;
   }
 }
 int input()
 {
   int x, y;
   x = digitalRead(BUTTON);
-  if (!x){
+  if (!x) {
     hang();
     return PAUSE;
   }
   x = analogRead(VX);
   y = analogRead(VY);
-  if (x < 400){
+  if (x < 400) {
     hang();
     return PREV;
   }
-  if (x > 600){
+  if (x > 600) {
     hang();
     return NEXT;
   }
-  if (y > 600){
+  if (y > 600) {
     hang();
     return VOL_UP;
   }
-  if (y < 400){
+  if (y < 400) {
     hang();
     return VOL_DN;
   }
@@ -112,27 +114,27 @@ int input()
 }
 void autonext()
 {
-  if(!music.isPlaying()){//如果这首歌播完了，选择播放的下一首
-      if(mode==0){//顺序播放
-        wavfile.close();
-        wavfile=dir.openNextFile();
-        cur++;
-        if(!wavfile){
-          cur=1;
-          wavfile=dir.rewindDirectory();
-        }
-      }else if(mode==2){//随机播放,有可能是同一首歌
-        wavfile.close();
-        randomSeed(analogRead(A2));
-        int temp;
-        temp=random(0,totalsong)+1;
-        cur=temp;
-        wavfile=dir.rewindDirectory();
-        while(--temp){
-          wavfile.close();
-          wavfile=dir.openNextFile();
-        }
+  if (!music.isPlaying()) { //如果这首歌播完了，选择播放的下一首
+    if (mode == 0) { //顺序播放
+      wavfile.close();
+      wavfile = dir.openNextFile();
+      cur++;
+      if (!wavfile) {
+        cur = 1;
+        wavfile = dir.rewindDirectory();
       }
-      music.play(wavfile.name());//单曲循环文件没有变化
+    } else if (mode == 2) { //随机播放,有可能是同一首歌
+      wavfile.close();
+      randomSeed(analogRead(A2));
+      int temp;
+      temp = random(0, totalsong) + 1;
+      cur = temp;
+      wavfile = dir.rewindDirectory();
+      while (--temp) {
+        wavfile.close();
+        wavfile = dir.openNextFile();
+      }
     }
+    music.play(wavfile.name());//单曲循环文件没有变化
+  }
 }
