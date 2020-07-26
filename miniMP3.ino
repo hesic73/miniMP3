6ff8a1dfacea7653ca74dca48da932add3ec61c5
hesic73 <hesicheng2001@163.com>
2020-07-26 12:35:58 +0800

    添加按键切换播放模式，但还没有做具体切歌

--- miniMP3.ino
+++ miniMP3.ino
@@ -1,10 +1,11 @@
 #include "SD.h"
 #include "TMRpcm.h"
 #include "SPI.h"
-#define SDcard 4
-#define SOUND 9
-#define BUTTON 2
-#define VX A0
+#define SDcard 4//SD卡模块
+#define SOUND 9//音频信号输出引脚
+#define BUTTON 2//摇杆的按键
+#define MODE 3//切换播放模式的按键,这个用外部中断实现
+#define VX A0 //摇杆方向
 #define VY A1
 //用户行为
 #define NOP -1
@@ -13,22 +14,66 @@
 #define NEXT 2//后一首
 #define VOL_UP 3//调大音量
 #define VOL_DN 4//减小音量
+#define MAX(x,y) ((x)>(y)?(x):(y))
+#define MIN(x,y) ((x)<(y)?(x):(y))
 File root;//根目录,为简便先假定该目录下全是.wav文件，之后可以考虑改为有次级目录
 File wavfile;//音频文件，需要获得它的文件名，不然无法实现按键切歌
-int mode;//一个饼，按键模块切换模式：单曲循环、顺序播放、随机播放
-int cur;//当前曲目
+char mode;//一个饼，按键模块切换模式：单曲循环1、顺序播放0、随机播放2
+char vol;//音量,用于显示
+unsigned int totalsong;//当前目录总曲目
+unsigned int cur;//当前曲目
 TMRpcm music;
-void hang()//等待摇杆回到初始状态
+void isr(){mode=(mode+1)%3;}//简单地切模式
+void hang();//等待摇杆回到初始状态
+int input();//摇动且回复到初始状态算一次输入
+void setup() {
+  //先初始化，然后播放第一首歌
+  pinMode(BUTTON,INPUT_PULLUP);
+  pinMode(MODE,INPUT_PULLUP);
+  mode=0;
+  attachInterrupt(1,isr,FALLING);
+  music.speakerPin = SOUND;
+  Serial.begin(9600);
+  if (!SD.begin(SDcard)) {
+    Serial.println("SD fail");
+    return;
+  }
+  vol=5;
+  music.setVolume(vol);   
+  music.quality(1);    
+  root=SD.open("/");
+  
+  wavfile=root.openNextFile() ;
+  if(!wavfile){
+    Serial.println("No file available.");
+    return ;
+  }
+  cur=1;
+  music.play(wavfile.name());//播放第一首曲子
+  wavfile.close();
+}
+
+void loop() {
+  switch (input()) {
+    case NOP: break;
+    case PAUSE: music.pause(); break;
+    case PREV: break;
+    case NEXT: break;
+    case VOL_UP: vol=MAX(vol+1,7);music.setVolume(vol);break;
+    case VOL_DN: vol=MIN(vol-1,0);music.setVolume(vol);break;
+  }
+}
+void hang()
 {
   int x,y,b;
-  while(1){//也要考虑等的过程中歌曲完毕的情况
+  while(1){//也要考虑等的过程中歌曲完毕的情况？
     b = digitalRead(BUTTON);
     x = analogRead(VX);
     y = analogRead(VY);
     if(b&&(x>400&&x<600)&&(y>400&&y<600)) break;
   }
 }
-int input()//摇动且回复到初始状态算一次输入
+int input()
 {
   int x, y;
   x = digitalRead(BUTTON);
@@ -55,36 +100,4 @@ int input()//摇动且回复到初始状态算一次输入
     return VOL_DN;
   }
   return NOP;
-}
-void setup() {
-  pinMode(BUTTON,INPUT_PULLUP);
-  music.speakerPin = SOUND;
-  Serial.begin(9600);
-  if (!SD.begin(SDcard)) {
-    Serial.println("SD fail");
-    return;
-  }
-  music.setVolume(5);    //   设置音量0 ~7
-  music.quality(1);    
-    
-  root=SD.open("/");
-  wavfile=root.openNextFile() ;
-  if(!wavfile){
-    Serial.println("No file available.");
-    return ;
-  }
-  cur=1;
-  music.play(wavfile.name());//播放第一首曲子
-  wavfile.close();
-}
-
-void loop() {
-  switch (input()) {
-    case NOP: break;
-    case PAUSE: music.pause(); break;
-    case PREV: break;
-    case NEXT: break;
-    case VOL_UP: music.volume(1);break;
-    case VOL_DN: music.volume(0);break;
-  }
 }
